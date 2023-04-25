046cf7b4c7f81e95ca33bb3ba57c37549f5025b6
Josiah Luikham <57018559+cimmerian62@users.noreply.github.com>
2023-04-25 11:10:34 -0400

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,5 +21,16 @@ app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("ht
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try {
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+} catch(Exception ex) {
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "an error occurred during migration");
+}
 
 app.Run();
